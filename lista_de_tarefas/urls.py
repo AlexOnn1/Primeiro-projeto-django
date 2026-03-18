b77577cb1a28c54241be6e3e05cb2717e911700c
Alexsander <alexsander.santos.contato@gmail.com>
2026-03-17 23:11:14 -0300

    Redirecionamento de rota com botão no hrtml usando funções django

--- lista_de_tarefas/urls.py
+++ lista_de_tarefas/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
+app_name="lista_de_tarefas"
+
 urlpatterns = [
     path("", views.tarefas_home),
-    path("adicionar/", views.tarefas_adicionar)
+    path("adicionar/", views.tarefas_adicionar, name="adicionar")
 ]
